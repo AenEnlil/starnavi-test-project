fd230bcd29cc2bdafa2fd540d8c07f743fc6efa9
AenEnlil <morphinegermanika@gmail.com>
2024-10-27 20:46:49 +0200

    implemented comment creation ai validation

--- app/comments/schemas.py
+++ app/comments/schemas.py
@@ -1,24 +1,45 @@
 from datetime import datetime
 from typing import Optional, List
 
-from pydantic import BaseModel, Field
+from fastapi import HTTPException
+from google.api_core.exceptions import ResourceExhausted
+from pydantic import BaseModel, Field, model_validator
+from starlette import status
 
+from app.config import get_settings
 from app.custom_fields import PyObjectId
+from app.vertex_ai_core.core import get_result_of_ai_validation
+from app.messages import AI_VALIDATION_ERROR, AI_REQUEST_QUOTA_EXCEEDED
 
 
 class CommentBaseSchema(BaseModel):
-    text: str
+    text: str = Field(min_length=3, max_length=1000)
 
 
-class CommentCreateInSchema(BaseModel):
-    text: str
+class CommentCreateInSchema(CommentBaseSchema):
+
+    @model_validator(mode='after')
+    def validate_attrs(self):
+        if get_settings().USE_AI_FOR_TEXT_VALIDATION:
+            try:
+                validation_result = get_result_of_ai_validation({"text": self.text})
+                if not validation_result.get('result'):
+                    failed_fields = validation_result.get('failed_fields')
+                    raise ValueError(f'following fields contains offensive language: {failed_fields}')
+            except ValueError as _e:
+                raise ValueError(_e.args[0])
+            except ResourceExhausted:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=AI_REQUEST_QUOTA_EXCEEDED)
+            except Exception as _e:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=AI_VALIDATION_ERROR)
+        return self
 
 
 class CommentUpdateSchema(CommentBaseSchema):
-    text: Optional[str] = Field(default=None, example='some text')
+    text: Optional[str] = Field(default=None, example='some text', min_length=3, max_length=1000)
 
 
-class CommentCreateSchema(CommentCreateInSchema):
+class CommentCreateSchema(CommentBaseSchema):
     post_id: PyObjectId
     author_id: PyObjectId
 
