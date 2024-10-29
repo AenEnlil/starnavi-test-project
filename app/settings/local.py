edf48c2574acb6f56f832d7c91fb6af464987eeb
AenEnlil <morphinegermanika@gmail.com>
2024-10-29 22:12:06 +0200

    settings updated

--- app/settings/local.py
+++ app/settings/local.py
@@ -3,7 +3,7 @@ from .base import BaseAppSettings
 
 class LocalAppSettings(BaseAppSettings):
     MONGO_URL: str
-    DATABASE_NAME: str
+    DATABASE_NAME: str = 'starnavi'
     LOGS_ENABLED: bool = True
 
     USE_AI_FOR_TEXT_VALIDATION: bool = True
